2b34b789f1c918185c2d7a7a4872e9c1fe549ec5
tony2tones <anthonymarques@outlook.com>
2021-01-04 14:38:53 +0200

    updated whole project to not have firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
